e1b7dee2ff53c3de9812356c1847058e7d1575fa
Yassine El Hadim <yel-hadi@c4r2p9.1337.ma>
2026-07-02 20:13:14 +0100

    push

--- pathfinder.py
+++ pathfinder.py
@@ -4,9 +4,9 @@ from parser import MapData, Zone, start
 from graph import Graph
 import math
 
-class Tracking_Data:
+class Tracking_Data():
     """All the Data needed to implement Dijkstra algorithm"""
-    def _init_(self, current, neighbors: List[str], distance: Dict[str, float], previous: Dict[str, None|Zone]) -> None:
+    def __init__(self, current: Zone, neighbors: List[str], distance: Dict[str, float], previous: Dict[str, None|Zone]) -> None:
         self.current = current
         self.neighbors = neighbors
         self.distance = distance
@@ -48,6 +48,7 @@ class PathFinder:
         if current is None:
             current: Zone = self.map_data.zones["start_hub"]
         previous: Dict[str, None|Zone] = {}
+        print(type(current))
         neighbors = self.graph.get_neighbors(current)
         for zone in zones:
             if self.map_data.zones[zone].zone_place == "start_hub":
@@ -61,20 +62,27 @@ class PathFinder:
         return Tracking_Data(current, neighbors, distance, previous)
 
 
-    def get_cl_zone(self) -> Zone:
-        tracked = self.set_data()
+    def get_cl_zone(self, current: Zone) -> Zone:
+        tracked = self.set_data(current)
         for neighbor in tracked.neighbors:
             zone_type = self.map_data.zones[neighbor].zone_type
             if zone_type == "normal" or zone_type == "priority":
                 tracked.distance[neighbor] = 1
             elif zone_type == "restricted":
                 tracked.distance[neighbor] = 2
-        # find from distance the smallest zones in a sorted list
-        # choose form the sorted list the smallest one that is unvisited
+        unvisited_zones = {}
+        for zone in tracked.distance:
+            if not self.is_visited(zone):
+                unvisited_zones[zone] = tracked.distance[zone]
+        closest_zone = min(unvisited_zones, key=unvisited_zones.get)
+        return map_data.zones[closest_zone]
             
 
 
 if __name__ == "__main__":
-    data = start("/home/linux_yassine/42_cursus/fly-in/maps/easy/01_linear_path.txt")
-    got_it = PathFinder(data)
-    got_it.set_data(data)
+    map_data = start("/home/yel-hadi/fly-in/maps/easy/01_linear_path.txt")
+    got_it = PathFinder(map_data)
+
+    zones = map_data.zones
+    for zone in zones:
+        print(f'The current zone is:{got_it.get_cl_zone(map_data.zones[zone])}\n')
