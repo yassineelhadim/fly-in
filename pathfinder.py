53bc9551e18b9efef9e0dd4acf7b2991a8695381
Yassine El Hadime <yassineelhadime7@gmail.com>
2026-07-12 14:23:54 +0100

    still working on the algo

--- pathfinder.py
+++ pathfinder.py
@@ -4,13 +4,6 @@ from parser import MapData, Zone, start
 from graph import Graph
 import math
 
-class Tracking_Data():
-    """All the Data needed to implement Dijkstra algorithm"""
-    def __init__(self, current: Zone, neighbors: List[str], distance: Dict[str, float], previous: Dict[str, None|Zone]) -> None:
-        self.current = current
-        self.neighbors = neighbors
-        self.distance = distance
-        self.previous = previous
 
 class PathFinder:
     """Finds the shortest paths on the drone network using Dijkstra"""
@@ -19,8 +12,7 @@ class PathFinder:
         self.map_data = map_data
         self.graph: Graph = Graph(map_data)
         self.visited_zones: List[str] = []
-        self.previous: Dict[str, Zone | None] = self.init_dis_a_pre()[1]
-        self.distance: Dict[str, float] = self.init_dis_a_pre()[0]
+        self.distance, self.previous = self.init_dis_a_pre()[0]
 
     def is_blocked(self, zone_name: Zone) -> bool:
         """Checks if a zone is blocked or not"""
@@ -56,31 +48,45 @@ class PathFinder:
             previous[zone] = None
         return [distance, previous]
 
-
-    def get_cl_zone(self, current: Zone) -> Zone:
-        tracked = self.set_data(current)
-        for neighbor in tracked.neighbors:
+    def update_neighbors(self, current: Zone) -> None:
+        neighbors = self.graph.get_neighbors(current)
+        for neighbor in neighbors:
             zone_type = self.map_data.zones[neighbor].zone_type
             if zone_type == "normal" or zone_type == "priority":
-                tracked.distance[neighbor] = 1
+                self.distance[neighbor] = 1
             elif zone_type == "restricted":
-                tracked.distance[neighbor] = 2
+                self.distance[neighbor] = 2
+
+    def get_cl_zone(self, current: Zone) -> Zone:
+        # ------this is for updating the distance, I will do it in another function-------------
+        # neighbors = self.graph.get_neighbors(current)
+        # for neighbor in neighbors:
+        #     zone_type = self.map_data.zones[neighbor].zone_type
+        #     if zone_type == "normal" or zone_type == "priority":
+        #         self.distance[neighbor] = 1
+        #     elif zone_type == "restricted":
+        #         self.distance[neighbor] = 2
         unvisited_zones = {}
-        for zone in tracked.distance:
+        for zone in self.distance:
             if not self.is_visited(zone):
-                unvisited_zones[zone] = tracked.distance[zone]
+                unvisited_zones[zone] = self.distance[zone]
         closest_zone = min(unvisited_zones, key=unvisited_zones.get)
         return map_data.zones[closest_zone]
 
     def dijkstra(self):
-        cur_zone = self.map_data.zones[]
-        closest = self.get_cl_zone()
+        zones = self.map_data.zones
+        for zone in zones:
+            if zones[zone].zone_place == "start":
+                current_place = "start"
+                current_zone = zones[zone].name
+        nb_zones = len(zones)
+        while len(self.visited_zones) < nb_zones:
+            closest_zone = self.get_cl_zone(current_zone)
+            self.visited_zones.append(closest_zone)
+            
+            
 
 
 if __name__ == "__main__":
     map_data = start("/home/yel-hadi/fly-in/maps/easy/01_linear_path.txt")
     got_it = PathFinder(map_data)
-
-    zones = map_data.zones
-    for zone in zones:
-        print(f'The current zone is:{got_it.get_cl_zone(map_data.zones[zone])}\n')
